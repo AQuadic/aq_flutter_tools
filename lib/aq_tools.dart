2946bf2da2f114ed8e9fe48567b7081c2a4d0282
Eslam El-feky <33692485+eslamira@users.noreply.github.com>
2022-02-08 13:11:18 +0200

    rm calc and test

--- lib/aq_tools.dart
+++ lib/aq_tools.dart
@@ -2,9 +2,3 @@ library aq_tools;
 
 export 'src/models/responsive_image_model.dart';
 export 'src/widgets/responsive_image_widget.dart';
-
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
