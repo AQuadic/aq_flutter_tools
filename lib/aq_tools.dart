6b75972b9f710b12a2a7eeda72f5bc7c9858bcd5
Eslam El-feky <33692485+eslamira@users.noreply.github.com>
2022-02-08 13:08:07 +0200

    export

--- lib/aq_tools.dart
+++ lib/aq_tools.dart
@@ -1,5 +1,8 @@
 library aq_tools;
 
+export 'src/models/responsive_image_model.dart';
+export 'src/widgets/responsive_image_widget.dart';
+
 /// A Calculator.
 class Calculator {
   /// Returns [value] plus 1.
