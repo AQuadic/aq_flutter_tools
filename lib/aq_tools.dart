18ea5e226258a9fa8d01cd64d69cf8f56d6e3249
Saifallak <Saifallak@yahoo.com>
2024-05-26 13:20:46 +0300

    LangModelJsonConverter

--- lib/aq_tools.dart
+++ lib/aq_tools.dart
@@ -1,5 +1,6 @@
 library aq_tools;
 
 export 'src/models/lang_model.dart';
+export 'src/models/lang_model_json_converter.dart';
 export 'src/models/responsive_image_model.dart';
 export 'src/widgets/responsive_image_widget.dart';
