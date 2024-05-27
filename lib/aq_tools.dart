48331bccffa0681d24b9a64e7339295312885ce9
Saifallak <Saifallak@yahoo.com>
2024-05-27 18:17:51 +0300

    export.

--- lib/aq_tools.dart
+++ lib/aq_tools.dart
@@ -1,6 +1,5 @@
 library aq_tools;
 
 export 'src/models/lang_model.dart';
-export 'src/models/lang_model_json_converter.dart';
 export 'src/models/responsive_image_model.dart';
 export 'src/widgets/responsive_image_widget.dart';
